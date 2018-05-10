76123bd429ed0d53812b7b0115bdc9f8028d41ed
Christopher Runyan <christopherrunyan@mac-mini.frontier.com>
2018-05-10 10:23:48 -0500

    Initial Commit

---
+++ Soundcloud Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soundcloud Test
+//
+//  Created by Christopher Runyan on 5/10/18.
+//  Copyright © 2018 Christopher Runyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
